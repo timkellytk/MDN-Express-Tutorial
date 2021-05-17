449e45f4a537b0ce771d233780c87756f7fd60dc
Tim Kelly <timkelly@4mations-MacBook-Pro.local>
2021-05-17 14:30:36 +1000

    Completed steps 1 - 10

--- models/genre.js
+++ models/genre.js
@@ -1,16 +1,3 @@
-/* 
-
-Open your ./models/genre.js file and create a schema for storing genres (the category of book, e.g. whether it is fiction or non-fiction, romance or military history, etc).
-
-The definition will be very similar to the other models:
-
-The model should have a String SchemaType called name to describe the genre.
-This name should be required and have between 3 and 100 characters.
-Declare a virtual for the genre's URL, named url.
-Export the model.
-
-*/
-
 var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
@@ -25,7 +12,7 @@ var GenreInstanceSchema = new Schema({
 });
 
 GenreInstanceSchema.virtual("url").get(function () {
-  return "/genre/" + this._id;
+  return "/catalog/genre/" + this._id;
 });
 
-module.exports = mongoose.model("GenreInstance", GenreInstanceSchema);
+module.exports = mongoose.model("Genre", GenreInstanceSchema);
