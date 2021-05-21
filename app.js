e0cf1bded2ad044fcf38d19af4115403c9809fed
Your Name <you@example.com>
2021-05-21 15:39:27 +1000

    added engine to package.json and set up environment variable for mongoDB for Heroku

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require("mongoose");
-var mongoDB =
-  "mongodb+srv://timtest1:test123@cluster0.eosvt.mongodb.net/local_library?retryWrites=true&w=majority";
+var dev_db_url =
+  "mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true";
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
