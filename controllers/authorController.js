797d78113697fe42ec07e2ec820e204b4205dcd2
Your Name <you@example.com>
2021-05-21 13:42:55 +1000

    Created the logic for author_update_post

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -201,6 +201,60 @@ exports.author_update_get = function (req, res, next) {
 };
 
 // Handle Author update on POST.
-exports.author_update_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Author update POST");
-};
+exports.author_update_post = [
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters"),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified")
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("author_form", {
+        title: "Update Author",
+        author: author,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    Author.findByIdAndUpdate(
+      req.params.id,
+      author,
+      {},
+      function (err, theauthor) {
+        if (err) {
+          return next(err);
+        }
+
+        res.redirect(theauthor.url);
+      }
+    );
+  },
+];
